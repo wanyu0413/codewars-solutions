ecf0853df4e71a216677d5718984f7744511a305
Wanyu Jiang <wanyu19930930@gmail.com>
2025-11-11 20:02:05 +0900

    hashtag generator.js

---
+++ javascript/5kyu/hashTagGenerator.js
@@ -0,0 +1,23 @@
+// The marketing team is spending way too much time typing in hashtags.
+// Let's help them with our own Hashtag Generator!
+
+// Here's the deal:
+
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized, and remaining letters lowercased.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+
+function generateHashtag(str) {
+  if (!str.trim()) return false;
+
+  const hashTag =
+    "#" +
+    str
+      .trim()
+      .split(/\s+/)
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join("");
+
+  return hashTag.length > 140 ? false : hashTag;
+}
